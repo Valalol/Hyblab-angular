e4cac45c7f085354caee47090c857a04cae2fc75
Valentin Esnault <valentin.esnault92@gmail.com>
2025-01-20 00:55:31 +0100

    first draft of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ContentData } from './models/content-data';
+import { SituationComponent } from './situation/situation.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        CommonModule,
+        SituationComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Hyblab-angular';
+export class AppComponent implements OnInit {
+    title = "Hyblab-angular L'Equipe";
+    data?: ContentData[];
+    selected_scenario?: ContentData;
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        this.fetchData().subscribe((response) => {
+            this.data = response;
+            console.log(this.data);
+        });
+    }
+
+    fetchData(): Observable<ContentData[]> {
+        return this.http.get<ContentData[]>('/assets/data.json');
+    }
+
+    scenario_cliked(scenario: ContentData): void {
+        this.selected_scenario = scenario;
+    }
 }
