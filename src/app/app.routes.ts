c89cc0e0155c0d3ad9c124a933f40fd06e844229
Maxime Tchernychev <maxime.tchernychev@etu.univ-nantes.fr>
2025-01-23 16:25:16 +0100

    ajout des credits (pour l'instant pas beau)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { RoleplayComponent } from './roleplay/roleplay.component';
 import { HomeComponent } from './home/home.component';
+import { CreditsComponent } from './credits/credits.component';
+import { MakingoffComponent } from './makingoff/makingoff.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'roleplay', component: RoleplayComponent }
+    { path: 'roleplay', component: RoleplayComponent },
+    { path: 'credits', component: CreditsComponent },
+    { path: 'makingoff', component: MakingoffComponent }
 ];
